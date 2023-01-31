3716c85a2e57534d3021867c10f8c96eb2d3c84d
Mickael JEGAT <moosic39@gmail.com>
2023-01-31 16:20:10 +0100

    update criterion spec

---
+++ spec/tests/models/Criterion.spec.ts
@@ -0,0 +1,10 @@
+import Criterion, { CalculationRuleType } from '@src/models/Criterion';
+
+describe('Criterion test', () => {
+  it('should recognize an instance of a criterion', () => {
+    const criterion = Criterion.new('NbOfBug', 10, 'Nb Of Bug', {
+      type: CalculationRuleType.NONE,
+    });
+    expect(Criterion.instanceOf(criterion)).toBe(true);
+  });
+});
